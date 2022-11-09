8f125120c0529c5422ea212ee12c7ba791ed0e58
night <yseddiq@gmail.com>
2022-11-09 16:36:43 -0500

    got basic stuff done for About section and got it to render with profile pic

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import profilePic from '../../assets/images/profile.jpg'
 
 function About(){
     return(
         <section>
-            
+            <img src={profilePic} />
+            <h1 id="about">This Is Who I Am</h1>
+            <p>I am an upcoming fullstack webdeveloper which means 
+               I can work on both front and back-end.</p>
         </section>
     )
 }
+
+export default About
