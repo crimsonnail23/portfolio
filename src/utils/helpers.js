fa246855bc5d24eee6380f5100d64995adab50e2
night <yseddiq@gmail.com>
2022-11-12 16:22:34 -0500

    added import statements in Contact.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
 export function validateEmail(email) {
     var regexEmailValidator = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    return regexEmailValidator.test(String(email
+    return regexEmailValidator.test(String(email).toLowerCase());
 }
