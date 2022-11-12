6fb4d9fc48a809241a299b98cca547cf8a2a1e01
night <yseddiq@gmail.com>
2022-11-12 16:38:54 -0500

    finished logic for handleChange and handleSubmit in Contact.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,6 +6,36 @@ function Contact() {
     const[errorMessage, setErrorMessage] =useState('');
     const {name,email,message} = formState;
 
+    const handleChange = (e) => {
+        //checks if email is valid/
+        if(e.target.name=== 'email'){
+            const validEmail=validateEmail(e.target.value);
+            //if email is not valid then run the following code.
+            if(!validEmail) {
+                setErrorMessage('Please enter a valid email address');
+            } else{
+                setErrorMessage('');
+            }
+        } else {
+            //if nothing is written in the name field then run the following code.
+            if(!e.target.value.length){
+                setErrorMessage(`${e.target.name} is required`)
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        setFormState({...formState, [e.target.name]: e.target.value})
+        if(!errorMessage){
+            setFormState({...formState, [e.target.name]: e.target.value})
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formState)
+    }
+
   return (
     <div className="container">
       <div className="row">
