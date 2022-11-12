4b48785ff73c161e156dfdc88c3f47a2c39ea1a3
night <yseddiq@gmail.com>
2022-11-12 16:30:08 -0500

    worked on handleChange logic in Contact.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,10 @@ import React, {useState} from "react";
 import {validateEmail} from '../../utils/helpers'
 
 function Contact() {
+    const[formState,setFormState] =useState({name:'', email: '', message:''});
+    const[errorMessage, setErrorMessage] =useState('');
+    const {name,email,message} = formState;
+
   return (
     <div className="container">
       <div className="row">
