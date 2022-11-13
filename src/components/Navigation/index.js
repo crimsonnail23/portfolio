e7854058f7ec8e20959742801939195b13972b6f
night <yseddiq@gmail.com>
2022-11-13 10:51:39 -0500

    removed commented out code from navigation bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -21,30 +21,6 @@ function Navigation({handlePageChange}){
             </Container>
         </Navbar>
         
-        // <nav>
-        //     <ul>
-        //         <li>
-        //             <a href="#about" onClick={()=>handlePageChange('about')}>
-        //                 About
-        //             </a>
-        //         </li>
-        //         <li>
-        //             <a href="#portfolio" onClick={()=>handlePageChange('portfolio')}>
-        //                 Portfolio
-        //             </a>
-        //         </li>
-        //         <li>
-        //             <a href="#contact" onClick={()=>handlePageChange('contact')}>
-        //                 Contact
-        //             </a>
-        //         </li>
-        //         <li>
-        //             <a href="#resume" onClick={()=>handlePageChange('resume')}>
-        //                 Resume
-        //             </a>
-        //         </li>
-        //     </ul>
-        // </nav>
     )
 }
 
